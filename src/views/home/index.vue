7f6e727563c341085e7fcee3b5549a95fe3ec266
zone <purecanee@gmail.com>
2024-05-28 02:51:20 +0800

    init

---
+++ src/views/home/index.vue
@@ -0,0 +1,21 @@
+<!-- 首页 -->
+<script setup>
+import Header from './components/FrontPage/header.vue'
+import HeaderDiff from './components/FrontPage/headerDiff.vue'
+import Menu from './components/FrontPage/menu.vue'
+import Footer from './components/FrontPage/footer.vue'
+import { DIFF_HOME_BANNER } from '@/config/index'
+import { useMainStore } from '@/store/index.js'
+const mainStore = useMainStore()
+onMounted(() => {
+  mainStore.getSettingConfig()
+  mainStore.setLanguage('en')
+})
+</script>
+
+<template>
+  <HeaderDiff v-if="DIFF_HOME_BANNER.includes(_getConfig('_APP_ENV'))"></HeaderDiff>
+  <Header v-else></Header>
+  <Menu></Menu>
+  <Footer></Footer>
+</template>
